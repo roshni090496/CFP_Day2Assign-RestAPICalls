d5fe299ecd4ccd291d3e3047487f44534cb41620
roshni090496 <rs09041001@gmail.com>
2022-07-07 16:50:37 -0700

    Added : Welcome Message

---
+++ src/main/java/com/example/day2assignrestapicalls/Day2AssignRestApiCallsApplication.java
@@ -0,0 +1,15 @@
+package com.example.day2assignrestapicalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2AssignRestApiCallsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day2AssignRestApiCallsApplication.class, args);
+        System.out.println("Hello");
+
+    }
+
+}
