c49500b4f03c8bb82281f8880e42bf1e598f6cb1
roshni090496 <rs09041001@gmail.com>
2022-07-07 17:27:52 -0700

    Added : Using Path Variable

--- src/main/java/com/example/day2assignrestapicalls/controller/HelloRestApi.java
+++ src/main/java/com/example/day2assignrestapicalls/controller/HelloRestApi.java
@@ -1,5 +1,6 @@
 package com.example.day2assignrestapicalls.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,10 @@ public class HelloRestApi {
     // UC1
     public String sayHello(){
         return "Hello From Bridgelabz";
+    }
+    // UC2
+    @RequestMapping ("/query/{name}")
+    public String sayHelloPath(@PathVariable String name) {
+        return "Hello " + name + "!";
     }
 }
